21c2dbd39370fce3183e46ac8bad71a2266cddd7
sarthaksonawane77 <sarthaksonawane112@gmail.com>
2025-07-14 18:21:31 +0530

    Update ReverseArray.java

--- ArrayProblems/ReverseArray.java
+++ ArrayProblems/ReverseArray.java
@@ -1,1 +1,23 @@
-hahaha
+public class practice {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter Array Size");
+        int size= scanner.nextInt();
+        int[] arr= new int[size];
+        System.out.println("Enter array elements");
+        for(int i=0;i<size;i++){
+            arr[i]= scanner.nextInt();
+        }
+        int temp;
+        for(int i=0;i<size/2;i++){
+            temp=arr[i];
+            arr[i]=arr[size-1-i];
+            arr[size-1-i]=temp;
+        }
+        for(int i=0;i<size;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+
